983d906eaba8c2eada8e05877caea88c412dd0ab
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2024-03-16 00:49:05 +0100

    update shadow

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -14,7 +14,6 @@ const Tab = ({tab, isFilterTab, isActiveTab, handleClick}) => {
         <img src={tab.icon}
         className={`${isFilterTab ? 'w-2/3 h-2/3' : 'w-11/12 h-11/12 object-contain'}`}
         />
-      
     </div>
   )
 }
